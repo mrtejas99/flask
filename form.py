2728ade09fc1228874f462c5664a4f433da92fa4
Narayan Bandodker <46126206+narayanPCMR@users.noreply.github.com>
2019-08-19 22:44:30 +0530

    Fixed form display, Added TODO PLEASE FIX form.py

--- form.py
+++ form.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request
-
 import sqlite3 as sql
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,7 +16,15 @@ def hello():
 def bye():
     f = request.form
     
-    return render_template("submit.html", formT=f)
+    name = f["project"]
+    
+    #TODO - Insert SQL Select query to fetch name, assignedTo and status
+    
+    database_name = ""
+    database_assignedto = ""
+    database_status = ""
+    
+    return render_template("submit.html", formT={"name": database_name, "assignedTo": database_assignedto, "status": database_status})
 
 if __name__=="__main__":
     app.run(debug=True)
