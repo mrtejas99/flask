f7bc2caccfe111a9a805d2810a6fc9d0702bb9cc
Narayan Bandodker <46126206+narayanPCMR@users.noreply.github.com>
2019-08-19 22:08:27 +0530

    Fixed List not showing, added sql to index

--- form.py
+++ form.py
@@ -1,14 +1,21 @@
 from flask import Flask,render_template
+import sqlite3 as sql
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return render_template("index.html")
+    conn = sql.connect('database.db')
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM project")
+    data = cur.fetchall()
+    conn.close()
+    return render_template("index.html", names=data)
 
 @app.route("/submit", methods=["POST"])
 def bye():
-	f = request.form
-	return render_template("submit.html", formT=f)
+    f = request.form
+    
+    return render_template("submit.html", formT=f)
 
 if __name__=="__main__":
     app.run(debug=True)
