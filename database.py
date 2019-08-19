1e3bd8d1bf06c82263d6580c43c864073e427815
Tejas Lotlikar <mrt3j45@gmail.com>
2019-08-19 21:58:17 +0530

    resolved database errors

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 import sqlite3 as sql
 conn = sql.connect('database.db')
-c = conn.cursor()
-c.execute("CREATE TABLE project (name text, assignedTo text, status text")
-c.execute("INSERT INTO project VALUES ('project1','bob','ongoing')")
-c.execute("INSERT INTO project VALUES ('project2','alice','completed')")
-c.execute("INSERT INTO project VALUES ('project3','tom','submitted')")
-c.execute("INSERT INTO project VALUES ('project4','jack','rejected')")
+print("opened successfully")
+
+conn.execute("CREATE TABLE project (name text, assignedTo text, status text)")
+conn.execute("INSERT INTO project VALUES ('project1','bob','ongoing')")
+conn.execute("INSERT INTO project VALUES ('project2','alice','completed')")
+conn.execute("INSERT INTO project VALUES ('project3','tom','submitted')")
+conn.execute("INSERT INTO project VALUES ('project4','jack','rejected')")
+print("table created and entries added")
 conn.commit()
+conn.close()
